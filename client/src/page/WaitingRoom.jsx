125975dea6e1775fa98483ae19b4f7918cebd01c
peppercatastrophe <abyansetia@gmail.com>
2023-12-21 09:54:06 +0700

    Feat: Add redux in WaitingRoom

--- client/src/page/WaitingRoom.jsx
+++ client/src/page/WaitingRoom.jsx
@@ -1,11 +1,17 @@
+import { useSelector } from "react-redux"
+
 export default function WaitingRoom() {
-    return (
-        <>
+
+  const roomCode = useSelector(state => state.redux.roomCode)
+  const players = useSelector(state => state.redux.players)
+  
+  return (
+    <>
         <Navbar/>
         <div className='container'>
       <div class="waiting-room-page">
         <h2 style={{color: "white"}}>Waiting Room...</h2>
-        <div style={{color: "#0ef"}}>Room Code : XYZA</div> <br />
+        <div style={{color: "#0ef"}}>Room Code : { roomCode ?? 'XYZA'}</div> <br />
         {/* <button className="btn btn-outline-secondary" disabled>-- Start Game --</button> */}
         <button className="btn btn-outline-success">-- Start Game --</button>
         <div>
@@ -16,17 +22,20 @@ export default function WaitingRoom() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>User1</td>
-              </tr>
-              <tr>
-                <td>User2</td>
-              </tr>
+              
+              { players && players?.map( (el, i) => {
+                return (
+                  <tr key={ i }>
+                    <td>{ el }</td>
+                  </tr>
+                )
+              })}
+              
             </tbody>
           </table>
         </div>
       </div>
       </div>
-        </>
-    )
+    </>
+  )
 }
